b39e8eec8bd21b60ec7ec8caa7a3c5a4c2add095
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2015-10-09 11:25:18 +0100

    A?adida url -/heroes/-

--- shield.com/main/urls.py
+++ shield.com/main/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^heroes/$', 'metahumans.views.view_all_heroes'),
 ]
+
+if settings.DEVELOPMENT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
