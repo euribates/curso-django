f74b8faaeda3d80fe5a0ace671a88aa65cf837e7
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2015-10-09 11:24:43 +0100

    A?adidos settings para MEDIA

--- shield.com/main/settings.py
+++ shield.com/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commons',
     'metahumans',
 )
 
@@ -101,3 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Files: Files uploaded by users
+
+MEDIA_URL = '/files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
